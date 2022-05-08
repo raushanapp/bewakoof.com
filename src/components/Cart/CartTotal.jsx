f4c2864cd15e19e8a7a08d0c4203bc10a4620a7a
abhi72181 <abhihits0404@gmail.com>
2022-05-08 18:49:46 +0530

    Abhishek final work done

--- src/components/Cart/CartTotal.jsx
+++ src/components/Cart/CartTotal.jsx
@@ -1,69 +1,66 @@
 import { useState, useEffect } from "react";
 import "./CartTotal.css";
 import { useNavigate } from "react-router-dom";
-
+import Button from "@mui/material/Button";
 export const CartTotal = () => {
-  const navigate=useNavigate()
+  const navigate = useNavigate();
   const [cartData, setCartData] = useState([]);
   const [actualPrice, setActualPrice] = useState(0);
   const [strikedPrice, setStrikedPrice] = useState(0);
-  const [discount,setDiscount]=useState(30/100)
-  const [redeem,setRedeem]=useState({
-    couponCode:""
-  })
-  const [check,setCheck]=useState(false)
+  const [discount, setDiscount] = useState(30 / 100);
+  const [redeem, setRedeem] = useState({
+    couponCode: "",
+  });
+  const [check, setCheck] = useState(false);
 
-  const handleChange=(e)=>{
-    console.log(e.target.value)
-    setRedeem({...redeem,[e.target.name]:e.target.value})
-    
-  }
-  const handleClick=()=>{
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setRedeem({ ...redeem, [e.target.name]: e.target.value });
+  };
+  const handleClick = () => {
     // console.log("check",redeem.couponCode)
-    if(redeem.couponCode==="Masai30"){
-      setCheck(true)
-      setActualPrice(Math.floor(actualPrice-(discount*actualPrice)))
+    if (redeem.couponCode === "Masai30") {
+      setCheck(true);
+      setActualPrice(Math.floor(actualPrice - discount * actualPrice));
+    } else {
+      setCheck(false);
     }
-    else{
-      setCheck(false)
-    }
-  }
+  };
 
-  const Proceed=()=>{
-    navigate("/Cart/payment")
-  }
-  console.log("redeem",redeem)
+  const Proceed = () => {
+    navigate("/Cart/payment");
+  };
+  console.log("redeem", redeem);
   const getUser = async () => {
     const response = await fetch("http://localhost:8080/cart");
     const data = await response.json();
     setCartData(data);
-    console.log("cart", data);
+    // console.log("cart", data);
     var sum = 0;
     var s = 0;
-    var a=0
-    var add=0
+    var a = 0;
+    var add = 0;
     data.map((e) => {
-      s = +(e.productcancelprice);
-      a=+(e.productprice)
+      s = +e.productcancelprice;
+      a = +e.productprice;
       console.log("s", s);
       sum += s;
-      add+=a
+      add += a;
     });
 
     console.log("sum", sum);
     setStrikedPrice(strikedPrice + sum);
-    setActualPrice(actualPrice+add)
+    setActualPrice(actualPrice + add);
     console.log("striked", strikedPrice);
     getUser();
   };
 
   useEffect(() => {
     getUser();
-    return function clean(){
-      console.log("cleaned")
-    }
+    return function clean() {
+      console.log("cleaned");
+    };
     // setCartData(data);
-
   }, []);
 
   return (
@@ -88,7 +85,9 @@ export const CartTotal = () => {
               onChange={handleChange}
               name="couponCode"
             />
-            <button className="redeem-btn" onClick={handleClick}>Redeem</button>
+            <button className="redeem-btn" onClick={handleClick}>
+              Redeem
+            </button>
           </div>
         </div>
         <div className="price-summary">
@@ -111,7 +110,9 @@ export const CartTotal = () => {
           <div className="pricedes-box">
             <div className="inside-pdb">
               <div className="price-des">Bag Discount</div>
-              <div className="actual-price">{`-₹ ${Math.abs(strikedPrice-actualPrice)}`}</div>
+              <div className="actual-price">{`-₹ ${Math.abs(
+                strikedPrice - actualPrice
+              )}`}</div>
             </div>
           </div>
           <div className="pricedes-box">
@@ -121,7 +122,9 @@ export const CartTotal = () => {
             </div>
           </div>
           <div className="saving">
-            <div className="inside-saving">{`You are saving ₹ ${Math.abs(strikedPrice-actualPrice)} on this order`}</div>
+            <div className="inside-saving">{`You are saving ₹ ${Math.abs(
+              strikedPrice - actualPrice
+            )} on this order`}</div>
           </div>
         </div>
         <div className="proceed-payment">
@@ -130,7 +133,21 @@ export const CartTotal = () => {
               <div>Total</div>
               <div>{`₹ ${actualPrice}`}</div>
             </div>
-            <button className="p-to-p-btn" onClick={Proceed}>Proceed to Payment</button>
+            {actualPrice ? (
+              <button className="p-to-p-btn" onClick={Proceed}>
+                Proceed to Payment
+              </button>
+            ) : (
+              <Button
+                sx={{
+                  width: 600,
+                }}
+                variant="contained"
+                disabled
+              >
+                Proceed to Payment
+              </Button>
+            )}
           </div>
         </div>
       </div>
